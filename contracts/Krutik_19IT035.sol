30bf8fa4fb37a099701352374273afae55b8c54c
KrutikGadhiya <krutikgadhiya@gmail.com>
2022-07-24 00:17:08 +0530

    section-3 video-6

--- contracts/Krutik_19IT035.sol
+++ contracts/Krutik_19IT035.sol
@@ -51,5 +51,22 @@ contract Krutik_19IT035 {
     }
 
     // transferfrom
-    function transferFrom() public {}
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+
+        // change the balance
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        // update the allowance
+        allowance[_from][msg.sender] -= _value;
+
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
 }
