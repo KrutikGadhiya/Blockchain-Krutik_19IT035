5a0254cf7992fd1ee4764c55fffdfb629582c05c
KrutikGadhiya <krutikgadhiya@gmail.com>
2022-07-23 23:26:14 +0530

    section-3 video-5

--- contracts/Krutik_19IT035.sol
+++ contracts/Krutik_19IT035.sol
@@ -7,14 +7,20 @@ contract Krutik_19IT035 {
     string public name = "Krutik_19IT035"; // token name
     string public standard = "Krutik_19IT035 v1.0"; // token standard
     string public symbol = "KG35"; // token symbol
+
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
 
     // consrtuctor to initialize supply with iniialSupply
     constructor(uint256 _initialSupply) {
         totalSupply = _initialSupply;
-
         // allocate initial supply
         balanceOf[msg.sender] = _initialSupply;
     }
@@ -26,14 +32,24 @@ contract Krutik_19IT035 {
     {
         // Exception if account doesn't have enough
         require(balanceOf[msg.sender] >= _value);
-
         balanceOf[msg.sender] -= _value;
         balanceOf[_to] += _value;
-
         // transfer event
         emit Transfer(msg.sender, _to, _value);
-
         // Returns a boolean
         return true;
     }
+
+    // approve
+    function approve(address _spender, uint256 _value)
+        public
+        returns (bool success)
+    {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    // transferfrom
+    function transferFrom() public {}
 }
--- test/Krutik_19IT035.js
+++ test/Krutik_19IT035.js
@@ -1,89 +1,125 @@
 let Krutik_19IT035 = artifacts.require("./Krutik_19IT035.sol");
 
 contract("Krutik_19IT035", (accounts) => {
-  it("initializes the contract with correct values", async () => {
-    let contract = await Krutik_19IT035.deployed();
-    let name = await contract.name();
-    let symbol = await contract.symbol();
-    let standard = await contract.standard();
+  // it("initializes the contract with correct values", async () => {
+  //   let contract = await Krutik_19IT035.deployed();
+  //   let name = await contract.name();
+  //   let symbol = await contract.symbol();
+  //   let standard = await contract.standard();
 
-    assert.equal(name, "Krutik_19IT035", "Check Name of Token");
-    assert.equal(symbol, "KG35", "Check Symbol of Token");
-    assert.equal(standard, "Krutik_19IT035 v1.0", "Check Standard of Token");
-  });
+  //   assert.equal(name, "Krutik_19IT035", "Check Name of Token");
+  //   assert.equal(symbol, "KG35", "Check Symbol of Token");
+  //   assert.equal(standard, "Krutik_19IT035 v1.0", "Check Standard of Token");
+  // });
 
-  it("check/allocates totalSupply onDeployment", async () => {
-    let contract = await Krutik_19IT035.deployed();
-    let totalSupply = await contract.totalSupply();
+  // it("check/allocates totalSupply onDeployment", async () => {
+  //   let contract = await Krutik_19IT035.deployed();
+  //   let totalSupply = await contract.totalSupply();
 
-    assert.equal(
-      totalSupply.toNumber(),
-      1000000,
-      "check totalSupply onDeployment"
-    );
+  //   assert.equal(
+  //     totalSupply.toNumber(),
+  //     1000000,
+  //     "check totalSupply onDeployment"
+  //   );
 
-    let adminAccount = await contract.balanceOf(accounts[0]);
-    assert.equal(
-      adminAccount.toNumber(),
-      1000000,
-      "allocates initial supply to admin"
-    );
-  });
+  //   let adminAccount = await contract.balanceOf(accounts[0]);
+  //   assert.equal(
+  //     adminAccount.toNumber(),
+  //     1000000,
+  //     "allocates initial supply to admin"
+  //   );
+  // });
 
-  it("transfer token ownership", async () => {
-    let contract = await Krutik_19IT035.deployed();
-    try {
-      const res = await contract.transfer.call(accounts[1], 999999999999999);
-      await assert.fail(res);
-    } catch (error) {
-      // console.log("[Message]: ", error.message);
-      assert(
-        error.message.includes("revert"),
-        "error message must contain revert"
-      );
-    }
-    const receipt = await contract.transfer(accounts[1], 250000, {
-      from: accounts[0],
-    });
+  // it("transfer token ownership", async () => {
+  //   let contract = await Krutik_19IT035.deployed();
+  //   try {
+  //     const res = await contract.transfer.call(accounts[1], 999999999999999);
+  //     await assert.fail(res);
+  //   } catch (error) {
+  //     // console.log("[Message]: ", error.message);
+  //     assert(
+  //       error.message.includes("revert"),
+  //       "error message must contain revert"
+  //     );
+  //   }
+  //   const receipt = await contract.transfer(accounts[1], 250000, {
+  //     from: accounts[0],
+  //   });
 
-    let success = await contract.transfer.call(accounts[1], 250000, {
-      from: accounts[0],
-    });
-    assert.equal(success, true, "it returns true");
+  //   let success = await contract.transfer.call(accounts[1], 250000, {
+  //     from: accounts[0],
+  //   });
+  //   assert.equal(success, true, "it returns true");
 
-    let balanceOf1 = await contract.balanceOf(accounts[1]);
-    assert.equal(
-      balanceOf1.toNumber(),
-      250000,
-      "adds amount to the receiving account"
-    );
-    let balanceOf0 = await contract.balanceOf(accounts[0]);
-    assert.equal(
-      balanceOf0.toNumber(),
-      750000,
-      "deducts amount from the sending account"
-    );
+  //   let balanceOf1 = await contract.balanceOf(accounts[1]);
+  //   assert.equal(
+  //     balanceOf1.toNumber(),
+  //     250000,
+  //     "adds amount to the receiving account"
+  //   );
+  //   let balanceOf0 = await contract.balanceOf(accounts[0]);
+  //   assert.equal(
+  //     balanceOf0.toNumber(),
+  //     750000,
+  //     "deducts amount from the sending account"
+  //   );
 
+  //   assert.equal(receipt.logs.length, 1, "triggers one event");
+  //   assert.equal(
+  //     receipt.logs[0].event,
+  //     "Transfer",
+  //     "should be the transfer event"
+  //   );
+  //   assert.equal(
+  //     receipt.logs[0].args._from,
+  //     accounts[0],
+  //     "log the account the tokens are trasgerred from"
+  //   );
+  //   assert.equal(
+  //     receipt.logs[0].args._to,
+  //     accounts[1],
+  //     "log the account the tokens are trasgerred to"
+  //   );
+  //   assert.equal(
+  //     receipt.logs[0].args._value.toNumber(),
+  //     250000,
+  //     "log the transfer amount"
+  //   );
+  // });
+
+  it("Approves tokens for delegate transfer", async () => {
+    const contract = await Krutik_19IT035.deployed();
+    let success = await contract.approve.call(accounts[1], 100);
+    assert.equal(success, true, "it return true");
+
+    let receipt = await contract.approve(accounts[1], 100);
     assert.equal(receipt.logs.length, 1, "triggers one event");
     assert.equal(
       receipt.logs[0].event,
-      "Transfer",
-      "should be the transfer event"
+      "Approval",
+      "should be the Approval event"
     );
     assert.equal(
-      receipt.logs[0].args._from,
+      receipt.logs[0].args._owner,
       accounts[0],
-      "log the account the tokens are trasgerred from"
+      "log the account the tokens are authorized by"
     );
     assert.equal(
-      receipt.logs[0].args._to,
+      receipt.logs[0].args._spender,
       accounts[1],
-      "log the account the tokens are trasgerred to"
+      "log the account the tokens are authorized to"
     );
     assert.equal(
       receipt.logs[0].args._value.toNumber(),
-      250000,
-      "log the transfer amount"
+      100,
+      "log the Approval amount"
+    );
+
+    let allowance = await contract.allowance(accounts[0], accounts[1]);
+    assert.equal(
+      allowance.toNumber(),
+      100,
+      "stores the allowance for delegate transfer"
     );
   });
 });
